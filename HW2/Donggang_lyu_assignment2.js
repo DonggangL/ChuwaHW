22a251b1ec43005bbb8b13bfbe43bc19db28d2bd
DonggangL <96214180+DonggangL@users.noreply.github.com>
2022-05-03 16:38:03 -0700

    Update Donggang_lyu_assignment2.js
    1. use reduce to sum
    2. put the string functions together

--- HW2/Donggang_lyu_assignment2.js
+++ HW2/Donggang_lyu_assignment2.js
@@ -1,35 +1,44 @@
-const itemObject = [
+let itemObject = [
   {quantity: 1, price: 200},
   {quantity: 3, price: 350},
   {quantity: 5, price: 400},
 ];
 let array = [];
+
 for (let i = 0; i < itemObject.length; i++) {
     const newQuantity = itemObject[i].quantity * 2;
     const newPrice = itemObject[i].price * 2;
     array.push({quantity: newQuantity, price: newPrice});
 }
+
+
 let array1 = [];
+let initialValue = 0;
 let sum = 0;
-itemObject.map(function(element, array) {
-    sum = sum + element.price;
-  if (element.quantity > 2 && element.price > 300) {
-      array1.push(element);
-  }
-});
-// console.log("original function is ");
-// console.log(itemObject);
+sum = itemObject.reduce(function (accumulator, cuurentV) {
+  return accumulator + cuurentV.quantity * cuurentV.price;
+}, initialValue);
+console.log(sum);
+// itemObject.map(function(element, array) {
+//     sum = sum + element.price * element.quantity;
+//   if (element.quantity > 2 && element.price > 300) {
+//       array1.push(element);
+//   }
+// });
+
 // console.log("Double the quantity and price is ");
 // console.log(array);
 // console.log("Find the value (q > 2 && p > 300) is ");
 // console.log(array1);
 // console.log("Total value is " + sum);
 
-const string = 'Perhaps The Easter-to-understand  Case    For Reduce Is   To Return The Sum Of All The Element   In   An    Array  ';
+const string = ' Perhaps The Easter-to-understand  Case    For Reduce Is   To Return The Sum Of All The Element   In   An    Array  ';
 
 const spaceRemove = string.replace(/\s+/g, ' ').trim();
-console.log(spaceRemove);
+// console.log(spaceRemove);
 const lowercase = spaceRemove.toLowerCase();
-console.log(lowercase);
+// console.log(lowercase);
 const removeAlphabet = lowercase.replace(/[^a-z0-9 ]/gi, ' ');
-console.log(removeAlphabet);
+// console.log(removeAlphabet);
+const result = string.replace(/[^a-z0-9 ]/gi, ' ').replace(/\s+/g, ' ').toLowerCase().trim();
+console.log(result);
